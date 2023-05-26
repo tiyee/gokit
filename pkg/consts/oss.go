84d449c95a2fc4d3e27176d2739ca15b5a1dc1a3
tiyee <tiyee@live.com>
2023-05-26 16:23:14 +0800

    feat:2.0.2

---
+++ pkg/consts/oss.go
@@ -0,0 +1,7 @@
+package consts
+
+const AccessKeyId = "<AccessKeyId>"
+const KeySecret = "<KeySecret>"
+const BaseUrl = "https://img.abc.cn"
+const HOST = "oss-cn-beijing-internal.aliyuncs.com"
+const BUCKET = "gokit"
