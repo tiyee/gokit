e5e6657aa6d98e675ecbf388c245184cb5b89f22
tiyee <tiyee@live.com>
2024-09-20 14:20:10 +0800

    feat: enhance dm

--- components/aliyun/direct_mail/email.go
+++ components/aliyun/direct_mail/email.go
@@ -13,6 +13,7 @@ type Client struct {
 	host           string
 	addr           string
 	replyToAddress string
+	from           string
 }
 type Option func(*Client)
 
@@ -23,6 +24,7 @@ func New(user, passwd, addr string, opts ...Option) *Client {
 		addr:           addr,
 		host:           strings.Split(addr, ":")[0],
 		replyToAddress: user, // default auth user
+		from:           "",
 	}
 	for _, opt := range opts {
 		opt(c)
@@ -37,7 +39,10 @@ func (c *Client) smtpAuth() smtp.Auth {
 // to: 收件人地址
 // cc: 抄送地址
 // bcc: 密送地址
-func (c *Client) SendEmail(subject, mailType, body string, to, cc, bcc []string) error {
+func (c *Client) SendEmail(subject, mailType, body string, to, cc, bcc []string, opts ...Option) error {
+	for _, opt := range opts {
+		opt(c)
+	}
 	auth := c.smtpAuth()
 	ccAddress := strings.Join(cc, ";")
 	bccAddress := strings.Join(bcc, ";")
@@ -48,7 +53,11 @@ func (c *Client) SendEmail(subject, mailType, body string, to, cc, bcc []string)
 		contentType = "Content-Type: text/html; charset=UTF-8"
 	}
 	replyToAddress := c.replyToAddress
-	msg := []byte("To: " + toAddress + "\r\nFrom: " + c.authUser + "\r\nSubject: " + subject + "\r\nDate: " + date + "\r\nReply-To: " + replyToAddress + "\r\nCc: " + ccAddress + "\r\nBcc: " + bccAddress + "\r\n" + contentType + "\r\n\r\n" + body)
+	from := c.authUser
+	if len(c.from) > 0 {
+		from = c.from
+	}
+	msg := []byte("To: " + toAddress + "\r\nFrom: " + from + "\r\nSubject: " + subject + "\r\nDate: " + date + "\r\nReply-To: " + replyToAddress + "\r\nCc: " + ccAddress + "\r\nBcc: " + bccAddress + "\r\n" + contentType + "\r\n\r\n" + body)
 	sendTo := slice.Merge(to, cc, bcc)
 	err := smtp.SendMail(c.addr, auth, c.authUser, sendTo, msg)
 	return err
